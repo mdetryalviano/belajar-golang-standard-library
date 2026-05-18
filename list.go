69d6e7f125d3a76c71566eb85fdaf82fd5e99a6a
mdetryalviano <mdetryalviano@gmail.com>
2026-05-18 22:17:47 +0700

    duration,list,ring,sort,time

---
+++ list.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	var data *list.List = list.New()
+
+	data.PushBack("Eko")
+	data.PushBack("Kurniawan")
+	data.PushBack("Khannedy")
+
+	var head *list.Element = data.Front()
+	fmt.Println(head.Value) // Eko
+
+	next := head.Next()
+	fmt.Println(next.Value) // Kurniawan
+
+	next = next.Next()
+	fmt.Println(next.Value) // Khannedy
+
+	for e := data.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+}
